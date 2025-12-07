bfbfc38cb3a87d966ab8b24fac810c2492b08999
ismail <ismailgarnaoui2005@gmail.com>
2025-12-07 23:25:56 +0100

    adding comments

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const video = document.getElementById("clip");
 video.playbackRate = 0.5;
 
-//la machine a ecrire
+//la liste des phrases a afficher
 const phrases = [
   "<span>R</span>EDEFINE <span>Y</span>OUR <span>L</span>IMITS",
   "<span>S</span>TRONGER <span>B</span>ODY, <span>S</span>HARPER <span>M</span>IND",
@@ -12,20 +12,26 @@ const phrases = [
 ];
 
 const titleEl = document.getElementById("rotating-title");
-let idx = 0;
+let idx = 0; //l'index de la phrase actuelle
 
+//lance animation de la machine a ecrire
 function setPhrase(i) {
-  titleEl.classList.remove("typewriter");
-  void titleEl.offsetWidth; // reflow to restart animation
-  titleEl.innerHTML = phrases[i];
-  titleEl.classList.add("typewriter");
+  titleEl.classList.remove("typewriter"); //enlever l'animation machine a ecrire pourque l'animation puisse redemarrer
+  void titleEl.offsetWidth; // forcer le navigateur à redemarrer l'animation
+  titleEl.innerHTML = phrases[i]; //changer le texte
+  titleEl.classList.add("typewriter"); //remet l'animation(typing et blink-caret)
 }
+setPhrase(idx); // afficher la premiere phrase
 
-setPhrase(idx);
+//5 secondes,le texte change
+//setInterval une fonction predefinie en js qui repete une action auto pendant un temps
 setInterval(() => {
-  idx = (idx + 1) % phrases.length;
+  idx = (idx + 1) % phrases.length; //passer a la phrase suivante
+  // %phrases.length pour revenir a 0 quand on arrive la fin
   setPhrase(idx);
-}, 5000);
+}, 5000); //changer toutes les 5 secondes
+
+// effet de flip pendant le scroll
 const flips = document.querySelectorAll(".flip");
 
 window.addEventListener("scroll", () => {
@@ -35,6 +41,8 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+//l effet de translation pendant le scroll
 const trsl = document.querySelectorAll(".translat");
 
 window.addEventListener("scroll", () => {
@@ -44,7 +52,10 @@ window.addEventListener("scroll", () => {
     }
   });
 });
-
+//menu toggle
 function toggleMenu() {
   document.getElementById("navMenu").classList.toggle("show");
 }
+/*la machine a ecrire 
+css :texte apparait petit à petit
+js change la phrase er relance l'animation*/
